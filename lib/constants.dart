5c58ede196641d81bb992dcb11b88d2865150b51
Benedikt <benediktsteinmueller@googlemail.com>
2021-11-21 20:53:16 +0100

    added basic app layout (not styled yet))

---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+//Colors for the main theme of the app
+
+const kBackgroundColorLight = Color(0xFFf8f8ff);
+const kBackgroundColorDark = Color(0xFFECECEC);
+const kAccentOne = Color(0xFF9790DD);
+const kAccentTwo = Color(0xFFB0C3AF);
+const kTextColorDark = Colors.black87;
+const kTextColorlight = Color(0xFFf8f8ff);
+
+const kHeadingTextStyle = TextStyle(
+    fontFamily: 'roboto',
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+    color: kTextColorDark);
+
+const kMainTextStyle = TextStyle(
+    fontFamily: 'roboto',
+    fontWeight: FontWeight.normal,
+    fontSize: 17.0,
+    color: kTextColorDark);
+
+
+const kInputLabelTextStyle = TextStyle(
+    fontFamily: 'roboto',
+    fontWeight: FontWeight.normal,
+    fontSize: 14.0,
+    color: kTextColorDark);
+
+const kMainButtonStyle = TextStyle(
+  fontFamily: 'roboto',
+    fontWeight: FontWeight.w800,
+    fontSize: 25.0,
+    color: kTextColorlight);
+
+
+
+
---
+++ lib/pages/StartPage.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../constants.dart';
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: Container(
+      color: kBackgroundColorLight,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(32.0, 64.0, 32.0, 80.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text("How long does it take Jeff Bezos to gain your net worth?",
+                style: kHeadingTextStyle),
+            SizedBox(
+              height: 12.0,
+            ),
+            Text(
+                "Enter your net worth and hear how long it takes for Jeff to earn the same amount",
+                style: kMainTextStyle),
+            SizedBox(
+              height: 22.0,
+            ),
+            Container(
+              color: kBackgroundColorDark,
+              child: new TextField(
+                decoration: new InputDecoration(
+                  hintStyle: TextStyle(fontSize: 17, color: kTextColorDark),
+                  hintText: 'Enter an amount',
+                  suffixIcon: Icon(Icons.money_sharp),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ], // Only numbers can be entered
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Container(
+              color: kAccentTwo,
+              child: MaterialButton(
+                child: Text(
+                  "Calculate",
+                  style: kMainButtonStyle,
+                ),
+                onPressed: () => {print("Button pressed")},
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
